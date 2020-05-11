3b3d66cce292380d27f31c5a7eaf5b12a5562fef
matheus <matheus.quintao.santiago@gmail.com>
2020-05-11 14:40:41 -0300

    Poc audio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, MaterialModule, BrowserAnimationsModule],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AudioService } from './services/audio.service';
+import { StreamState } from './interfaces/stream-state';
+
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +10,39 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  public audio = new Audio();
+  private state: StreamState;
 
+  constructor(private audioService: AudioService) {
+    this.audioService.getState().subscribe((state) => {
+      this.state = state;
+    });
+    this.playStream();
+  }
+
+  playStream() {
+    this.audioService.playStream('../../assets/music.mp3').subscribe(events => {
+    });
+  }
+
+  public play() {
+    if (this.state.playing) {
+      this.audioService.pause();
+    } else {
+      this.audioService.play();
+    }
+  }
+
+  public rewind() {
+    this.audioService.seekTo(this.state.currentTime - 10);
+  }
+
+  public delete() {
+    this.audio.src = '';
+  }
+
+  // public load() {
+  //   this.audio.src =  ;
+  //   this.audio.load();
+  // }
 }
